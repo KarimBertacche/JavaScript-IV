978790cc1aa7adf801af6a9374de6a006c983789
Karim B <mattiakarim@live.it>
2019-05-09 13:45:39 +0200

    Complete Person, Instructor, and Student classes with both attributes and methods and linked them to each other

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,77 @@
 // CODE here for your Lambda Classes
+// First we need a Person class. This will be our base-class
+// Person receives name age location gender all as props
+// Person receives speak as a method.
+// This method logs out a phrase Hello my name is Fred, I am from Bedrock where name and location are the object's own props
+
+class Person {
+    constructor(attributes) {
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+        this.gender = attributes.gender;
+    }
+
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
+}
+
+// Now that we have a Person as our base class, we'll build our Instructor class.
+// Instructor uses the same attributes that have been set up by Person
+// Instructor has the following unique props:
+// specialty what the Instructor is good at i.e. 'redux'
+// favLanguage i.e. 'JavaScript, Python, Elm etc.'
+// catchPhrase i.e. Don't forget the homies
+// Instructor has the following methods:
+// demo receives a subject string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
+// grade receives a student object and a subject string as arguments and logs out '{student.name} receives a perfect score on {subject}'
+
+class Instructor extends Person {
+    constructor(attributes) {
+        super(attributes);
+        this.specialty = attributes.specialty;
+        this.favLanguage = attributes.favLanguage;
+        this.catchPhrase = attributes.catchPhrase;
+    }
+
+    demo(subject) {
+        return `Today we are learning about ${subject}`
+    }
+
+    grade(studentObj, subject) {
+        return `{studentObj.name} receives a perfect score on {subject}`
+    }
+}
+
+// Now we need some students!
+// Student uses the same attributes that have been set up by Person
+// Student has the following unique props:
+// previousBackground i.e. what the Student used to do before Lambda School
+// className i.e. CS132
+// favSubjects. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
+// Student has the following methods:
+// listsSubjects a method that logs out all of the student's favoriteSubjects one by one.
+// PRAssignment a method that receives a subject as an argument and logs out that the student.name has submitted a PR for {subject}
+// sprintChallenge similar to PRAssignment but logs out student.name has begun sprint challenge on {subject}
+
+class Student extends Person {
+    constructor(attributes) {
+        super(attributes);
+        this.previousBackground = attributes.previousBackground;
+        this.className = attributes.className;
+        this.favSubject = attributes.favSubject;
+    }
+
+    listsSubjects() {
+        return this.favSubject.forEach(el => console.log(el));
+    }
+
+    PRAssignment(subject) {
+        return `${this.name} has submitted a PR for ${subject}`
+    }
+
+    sprintChallenge(subject) {
+        return `${this.name} has begun sprint challenge on ${subject}`
+    }
+}
